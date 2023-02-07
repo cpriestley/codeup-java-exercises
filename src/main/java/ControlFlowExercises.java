f219bc88a51d780cc4abae9f1414db969aecd1df
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-07 15:58:41 -0600

    ControlFlowExercises - do-while loop - refactor as for loop

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,9 +1,12 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while(i <= 15) {
-            System.out.println(i++);
+        for (int i = 5; i <= 15; i++) {
+            System.out.println(i);
+        }
+
+        for (long j = 2; j < 1000000; j*=j) {
+            System.out.println(j);
         }
 
     }

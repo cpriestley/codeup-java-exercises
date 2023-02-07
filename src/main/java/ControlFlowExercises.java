2ea3f5621b095d4a7c4e8c3ec80bcaebd1595eea
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-07 15:39:12 -0600

    ControlFlowExercises - do-while loop - Alter your loop to count backwards by 5's from 100 to -10.

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -4,13 +4,12 @@ public class ControlFlowExercises {
             System.out.println(i);
         }
 
-        int i = 0;
+        int i = 100;
         do {
             System.out.println(i);
-            i += 2;
-        } while (i <= 100);
+            i -= 5;
+        } while (i >= -10);
 
-// - Alter your loop to count backwards by 5's from 100 to -10.
 // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
     }
 }

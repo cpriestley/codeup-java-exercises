ee585c3c6bc29874ec77a411e18806b648d7a506
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-07 15:30:08 -0600

    ConsoleExercises - Prompt a user to enter an integer and store that value in an int variable using the nextInt method. - Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline. - Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user. - Prompt the user to enter values of length and width - Display the area and perimeter of that classroom.

---
+++ src/main/java/ConsoleExercises.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f.%n", pi);
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter an integer: ");
+        int userInt = scanner.nextInt();
+        System.out.format("You entered %d", userInt);
+
+        System.out.println("Please enter three words");
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.format("You entered: %s%n%s%n%s%n", word1, word2, word3);
+
+        System.out.println("Please enter a sentence");
+
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
+
+        System.out.format("Please enter the length and width of the Codeup classroom%nLength: ");
+        String length = scanner.nextLine();
+        System.out.print("Width: ");
+        String width = scanner.nextLine();
+        float area = Float.parseFloat(length) * Float.parseFloat(width);
+        float perimeter = Float.parseFloat(length) * 2 + Float.parseFloat(width) * 2;
+        System.out.format("The perimeter is %.2f feet and the area is %.2f square feet.", area, perimeter);
+        scanner.close();
+    }
+}

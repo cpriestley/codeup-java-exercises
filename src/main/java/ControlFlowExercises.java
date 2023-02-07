4ff7116e2acd36aab5ea2f84a632a3575e92845a
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-07 15:53:37 -0600

    ControlFlowExercises - while loop - revision - previously implemented as a for loop, revising to while loop.

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,14 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 5; i <= 15; i++) {
-            System.out.println(i);
-        }
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        } while (i < 1000000);
+        int i = 5;
+        while(i <= 15) {
+            System.out.println(i++);
+        }
 
     }
 }

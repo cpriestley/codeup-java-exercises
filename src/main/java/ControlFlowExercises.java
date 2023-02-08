50d7ab7ea969488f25b04ae036a992e58126c909
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-08 12:34:28 -0600

    ControlFlowExercises refactor - add text descriptions prior to loop executions

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,10 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+
+        System.out.format("Print 5 to 15:%n");
         for (int i = 5; i <= 15; i++) {
             System.out.println(i);
         }
 
+        System.out.format("%nPrint Squares From 2 While value is less than 1,000,000:%n");
         for (long j = 2; j < 1000000; j*=j) {
             System.out.println(j);
         }

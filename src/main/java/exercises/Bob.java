cfa986963987574d275ed8d85d24db2d3fc1eec0
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-13 23:17:58 -0600

    String Exercises - implement StringExercise class that outputs formatted strings using a single variable - implement Bob class that responds to user input as a lackadaisical teenager

--- src/main/java/exercises/Bob.java
+++ src/main/java/exercises/Bob.java
@@ -1,17 +1,18 @@
+package exercises;
+
 import java.util.Scanner;
 
 /**
- * Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+ * exercises.Bob is a lackadaisical teenager. In conversation, his responses are very limited.
  */
 public class Bob {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, my name is Bob.");
+        System.out.println("Hello, my name is exercises.Bob.");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.format("^ That's Bob. What would you like to say to Bob?%nSay 'bye' to end the conversation.%nYou: ");
-
+        System.out.format("^ That's exercises.Bob. What would you like to say to exercises.Bob?%nSay 'bye' to end the conversation.%nYou: ");
         while (true) {
             String userInput = scanner.nextLine();
             bobResponse(userInput);
@@ -26,20 +27,20 @@ public class Bob {
     }
 
     /**
-     * Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+     * exercises.Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
      * He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
      * He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
      * He answers 'Whatever.' to anything else.
      */
     public static void bobResponse(String input) {
         if (input.endsWith("?")) {
-            System.out.println("Bob: Sure.");
+            System.out.println("exercises.Bob: Sure.");
         } else if (input.endsWith("!")) {
-            System.out.println("Bob: Whoa, chill out!");
+            System.out.println("exercises.Bob: Whoa, chill out!");
         } else if (input.isEmpty()) {
-            System.out.println("Bob: Fine. Be that way!");
+            System.out.println("exercises.Bob: Fine. Be that way!");
         } else {
-            System.out.println("Bob: Whatever.");
+            System.out.println("exercises.Bob: Whatever.");
         }
     }
 }
--- src/main/java/exercises/StringExercise.java
+++ src/main/java/exercises/StringExercise.java
@@ -1,3 +1,5 @@
+package exercises;
+
 public class StringExercise {
     public static void main(String[] args) {
 

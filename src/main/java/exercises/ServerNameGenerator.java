dcfe5376743721912350ccf9e5313521e42aac04
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-15 12:20:57 -0600

    ServerNameGenerator refactor - replace Math.random() with Random()

--- src/main/java/exercises/ServerNameGenerator.java
+++ src/main/java/exercises/ServerNameGenerator.java
@@ -1,8 +1,16 @@
 package exercises;
 
+import java.util.Random;
+
 public class ServerNameGenerator {
     String[] adjectives = {"big", "small", "red", "blue", "green", "yellow", "purple", "orange", "black", "white"};
     String[] nouns = {"dog", "cat", "bird", "fish", "mouse", "snake", "lizard", "frog", "turtle", "hamster"};
+
+    Random random;
+
+    public ServerNameGenerator() {
+        random = new Random();
+    }
 
     public static void main(String[] args) {
         ServerNameGenerator serverNameGenerator = new ServerNameGenerator();
@@ -12,7 +20,7 @@ public class ServerNameGenerator {
     }
 
     private String randomElement(String[] words) {
-        int randomIndex = (int) (Math.random() * words.length);
+        int randomIndex = random.nextInt(words.length);
         return words[randomIndex];
     }
 }
